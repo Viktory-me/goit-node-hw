3eaab4f36afe3cd3173c360d91d0af7675162d31
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-10-10 19:09:12 +0300

    result correction

--- controllers/auth/avatars.js
+++ controllers/auth/avatars.js
@@ -1,5 +1,6 @@
 const { User } = require('../../models');
 const UploadAVatarService = require('../../services/local-upload');
+const { sendSuccessRes } = require('../../helpers');
 
 const avatars = async (req, res, next) => {
   try {
@@ -7,7 +8,7 @@ const avatars = async (req, res, next) => {
     const tmp = new UploadAVatarService(process.env.AVATAR_OF_USERS);
     const avatarURL = await tmp.saveAvatar({ idUser: id, file: req.file });
     await User.updateOne({ _id: id }, { avatar: avatarURL });
-    res.json({ message: 'done' });
+    sendSuccessRes(res, { avatarURL });
   } catch (error) {
     next(error);
   }
