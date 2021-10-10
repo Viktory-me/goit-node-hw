31429f5355474ae75d70ac2f7cca53e84dfab555
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-10-10 19:54:14 +0300

    add pagination

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,7 +2,13 @@ const { Contact } = require('../../models');
 const { sendSuccessRes } = require('../../helpers');
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find({}, '_id name email phone favorite');
+  console.log(req.query);
+  const { page = 1, limit = 3 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({}, '_id name email phone favorite', {
+    skip,
+    limit: +limit,
+  });
   sendSuccessRes(res, { result });
 };
 
